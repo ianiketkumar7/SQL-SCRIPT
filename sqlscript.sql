2b7dfee4dbbae1134e4e36483addf76ac6e10e6f
Aniket kumar <113085306+ianiketkumar7@users.noreply.github.com>
2023-02-06 15:12:24 +0530

    Add files via upload

---
+++ sqlscript.sql
@@ -0,0 +1,227 @@
+REM   Script: Session 02
+REM   Table prac 2
+
+create table STUDENT 
+( 
+    Roll no int, 
+    name varchar(30) 
+    dept varchar(30) 
+    no_of_books_issued int 
+);
+
+create table STUDENT 
+( 
+    Roll no int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int, 
+);
+
+create table STUDENT 
+( 
+    Roll no int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+create table STUDENT 
+( 
+    Roll no int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+create table STUDENT 
+( 
+    Rollno int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+create table STUDENT 
+( 
+    Rollno int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+insert into student values( 
+    101,'abc','cse',3 
+);
+
+create table STUDENT 
+( 
+    Rollno int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+insert into student values( 
+    101,'abc','cse',3 
+);
+
+select*from student;
+
+create table STUDENT 
+( 
+    Rollno int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+insert into student values( 
+    101,'abc','cse',3 
+);
+
+select*from 
+       student;
+
+create table STUDENT 
+( 
+    Rollno int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+insert into student values( 
+    101,'abc','cse',3 
+);
+
+select*from 
+       student;
+
+create table STUDENT 
+( 
+    Rollno int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+insert into student values( 
+    101,'abc','cse',3 
+);
+
+select*from 
+       student;
+
+create table STUDENT 
+( 
+    Rollno int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+insert into student values( 
+    101,'abc','cse',3 
+);
+
+select*from 
+       student;
+
+insert into student values(101,'abc','cse',3);
+
+select*from 
+       student;
+
+create table STUDENT 
+( 
+    Rollno int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+insert into student values(101,'Tharun','cse',3);
+
+insert into student values(102,'Nike','cse',5);
+
+insert into student values(103,'Cadet','NCC',7);
+
+insert into student values(104,'LOL BOY','DSW',2);
+
+insert into student values(105,'Nithish','12+',5);
+
+select*from 
+       student 
+;
+
+insert into student values(103,'Cadet','NCC',7);
+
+select*from 
+       student;
+
+insert into student values(105,'Nithish','12+',5);
+
+select*from 
+       student 
+;
+
+create table STUDENT 
+( 
+    Rollno int, 
+    name varchar(30), 
+    dept varchar(30), 
+    no_of_books_issued int 
+);
+
+insert into student values(101,'Tharun','cse',3);
+
+insert into student values(102,'Nike','cse',5);
+
+insert into student values(103,'Cadet','NCC',7);
+
+insert into student values(104,'LOL BOY','DSW',2);
+
+insert into student values(105,'Nithish','12+',5);
+
+select*from 
+       student 
+DESC STUDENT;
+
+DELETE FROM STUDENT WHERE ROLL NO=101;
+
+DELETE FROM STUDENT WHERE ROLL NO=101;
+
+DELETE FROM STUDENT WHERE ROLL NO=101;
+
+DELETE FROM STUDENT WHERE ROLLNO =101;
+
+select*from 
+       student;
+
+DELETE FROM STUDENT WHERE ROLLNO = 105;
+
+DELETE FROM STUDENT WHERE ROLLNO = 102;
+
+select*from 
+       student;
+
+DELETE FROM STUDENT WHERE ROLLNO = 104;
+
+DELETE FROM STUDENT WHERE ROLLNO = 103;
+
+select*from 
+       student;
+
+insert into student values(101,'Tharun','cse',3);
+
+insert into student values(102,'Nike','cse',5);
+
+insert into student values(103,'Cadet','NCC',7);
+
+insert into student values(104,'LOL BOY','DSW',2);
+
+insert into student values(105,'Nithish','12+',5);
+
+select*from 
+       student;
+
